005f48441510eced1149c88e80328c0a709aadfc
namtar <matthias.drummer@gmail.com>
2014-12-10 23:45:11 +0100

    basic project setup

---
+++ src/main/java/de/htw/berlin/student/gespic/Main.java
@@ -0,0 +1,13 @@
+package de.htw.berlin.student.gespic;
+
+/**
+ * Some Main class for the project.
+ *
+ * @author by Matthias Drummer on 10.12.2014
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // do nothing for the moment
+    }
+}
